f216d5c3aa358c24588d9fe7692b8523a0021448
aruanfreedom <aruanfreedom@outlook.com>
2016-12-21 03:00:06 +0700

    sas

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('scss/*.scss')
+      .pipe(sass({
+          outputStyle: 'compressed',
+          includePaths: ['node_modules/susy/sass']
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('css/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/*.scss', ['sass']);
+});
